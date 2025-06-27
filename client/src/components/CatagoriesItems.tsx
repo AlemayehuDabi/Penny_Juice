a6b170432819174e4bdc14e8e409f53766c7a8b9
Alemayehu <alemayehudabi606@gmail.com>
2025-06-27 10:27:21 +0300

    FEAT: catagories items ui

--- client/src/components/CatagoriesItems.tsx
+++ client/src/components/CatagoriesItems.tsx
@@ -1,7 +1,45 @@
+import logo from "../image/banner2.png";
+
 export const CatagoriesItems = () => {
   return (
-    <div>
-      <p>CatagoriesItems</p>
+    <div className="min-h-3xl mt-10 flex items-center justify-center ">
+      <div className="flex gap-6 items-center justify-center w-full">
+        {/* Left Image */}
+        <div className="rounded w-1/4 h-[34rem] border border-amber-400 transform hover:scale-105 transition duration-300">
+          <img
+            src={logo}
+            alt="Category Left"
+            className="w-full h-full object-cover rounded-lg"
+          />
+        </div>
+
+        {/* Two Stacked Center Images */}
+        <div className="flex flex-col gap-6 w-1/4 h-[34rem]">
+          <div className="w-full h-1/2 rounded  border border-amber-400 overflow-hidden transform hover:scale-105 transition duration-300">
+            <img
+              src={logo}
+              alt="Category Center Top"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div className="w-full h-1/2 rounded border border-amber-400 overflow-hidden transform hover:scale-105 transition duration-300">
+            <img
+              src={logo}
+              alt="Category Center Bottom"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </div>
+
+        {/* Right Image */}
+        <div className="rounded w-1/4 h-[34rem] border border-amber-400 transform hover:scale-105 transition duration-300">
+          <img
+            src={logo}
+            alt="Category Right"
+            className="w-full h-full object-cover rounded-lg"
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,12 +8,12 @@ import { Catagories } from "../components/Cartagories";
 
 export default function Home() {
   return (
-    <div className="min-h-screen relative px-4 mb-10 tracking-wide">
+    <div className="min-h-screen relative px-4 mb-96 tracking-wide">
       <div className="absolute top-10 inset-x-0 -z-10">
         <img
           src={splash}
           alt="back_img"
-          className="w-full h-full object-cover blur-md opacity-60"
+          className="w-full h-full object-cover blur-sm  opacity-60 "
         />
       </div>
       <div className="container mx-auto px-4 space-y-3">
